131243e0550815ca108e322e0a8254bd9ecdbdf4
Daniel De Alday <danieldealday@gmail.com>
2018-06-13 21:47:06 -0700

    current feature build to be shared across devices, working with immutability

--- src/components/Views/Jobs.js
+++ src/components/Views/Jobs.js
@@ -4,13 +4,16 @@ import { Consumer } from "../../Context";
 import Job from "../Job";
 
 const Jobs = () => {
+  // * state is transformed deeply to JavaScript from immutable Map
   return (
     <Consumer>
       {({ state, actions }) => {
+        const appdata = state.toJS();
         return (
           <Fragment>
             <Typography variant="display1">Jobs</Typography>
             <Job />
+            <Typography variant="body1">{JSON.stringify(appdata)}</Typography>
           </Fragment>
         );
       }}
--- src/initialState.js
+++ src/initialState.js
@@ -1,6 +1,20 @@
-const initialState = {
+import { Map, List } from "immutable";
+
+const initialState = new Map({
   name: "ssj daniel",
   age: 30
-};
+});
+
+// TODO: determine job specs - salary rate, company, position, current interview date, interview status
+
+// let job = {
+//   created: created,
+//   lastEditted: lastEditted,
+//   interviewStatus: interviewStats,
+//   interviewDate: interviewDate,
+//   companyName: companyName,
+//   jobTitle: jobTitle,
+//   salarayRate: salaryRate,
+// }
 
 export default initialState;
