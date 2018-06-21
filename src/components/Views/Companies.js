28179c84d5344a6eaf42f6a99889aca928bbebf6
Daniel De Alday <danieldealday@gmail.com>
2018-06-21 14:27:33 -0700

    added better structure to job component

---
+++ src/components/Views/Companies.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const Companies = () => <Typography variant="display1">Companies</Typography>;
+
+export default Companies;
--- src/components/Views/index.js
+++ src/components/Views/index.js
@@ -2,5 +2,6 @@ import Home from "./Home.js";
 import Schedule from "./Schedule.js";
 import Jobs from "./Jobs.js";
 import Contacts from "./Contacts.js";
+import Companies from "./Companies.js";
 
-export { Home, Schedule, Jobs, Contacts };
+export { Home, Schedule, Jobs, Contacts, Companies };
