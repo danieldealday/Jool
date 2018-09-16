0eb001fc74b16c7859b7e01930af67bae9be22f7
Daniel De Alday <danieldealday@gmail.com>
2018-09-15 17:26:34 -0700

    updated for windows and added change handler for update dialog

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import {
   FormControl,
   InputLabel,
@@ -7,84 +7,96 @@ import {
   Typography
 } from "@material-ui/core";
 
-const Form = (props) => {
-  const data = props.data.toJS();
-  return (
-    <Fragment>
-      <Button>Job Status</Button>
-      <Typography variant="body1">
-        {data.jobTitle}
-      </Typography>
-      <FormControl >
-        <InputLabel htmlFor="jobTitle">Job Title</InputLabel>
-        <Input
-          id="jobTitle"
-          name="jobTitle"
-          value={data.jobTitle}
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="companyName">Company Name</InputLabel>
-        <Input
-          id="companyName"
-          name="companyName"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="location">Location</InputLabel>
-        <Input
-          id="location"
-          name="location"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="salary">Salary</InputLabel>
-        <Input
-          id="salary"
-          name="salary"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="applicationDate">Application Date</InputLabel>
-        <Input
-          id="applicationDate"
-          name="applicationDate"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="nextAppointment">Next Appointment</InputLabel>
-        <Input
-          id="nextAppointment"
-          name="nextAppointment"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="email">Email</InputLabel>
-        <Input
-          id="email"
-          name="email"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="phone">Phone</InputLabel>
-        <Input
-          id="phone"
-          name="phone"
-        />
-      </FormControl>
-      <FormControl >
-        <InputLabel htmlFor="notes">Notes</InputLabel>
-        <Input
-          id="notes"
-          name="notes"
-          multitline
-          fullWidth={true}
-          rowsMax={20}
-          rows={20}
-        />
-      </FormControl>
-    </Fragment>
-  );
+class Form extends Component {
+
+  handleChange = e => {
+    const modState = {};
+    modState["editted"] = true;
+    modState[e.target.name] = e.target.value;
+    this.setState((prevState, props) => Object.assign(prevState, modState));
+  };
+  
+  render () {
+    const data = this.props.data.toJS();
+
+    return (
+      <Fragment>
+        <Button>Job Status</Button>
+        <Typography variant="body1">
+          {data.jobTitle}
+        </Typography>
+        <FormControl >
+          <InputLabel htmlFor="jobTitle">Job Title</InputLabel>
+          <Input
+            id="jobTitle"
+            name="jobTitle"
+            onChange={this.handleChange}
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="companyName">Company Name</InputLabel>
+          <Input
+            id="companyName"
+            name="companyName"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="location">Location</InputLabel>
+          <Input
+            id="location"
+            name="location"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="salary">Salary</InputLabel>
+          <Input
+            id="salary"
+            name="salary"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="applicationDate">Application Date</InputLabel>
+          <Input
+            id="applicationDate"
+            name="applicationDate"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="nextAppointment">Next Appointment</InputLabel>
+          <Input
+            id="nextAppointment"
+            name="nextAppointment"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input
+            id="email"
+            name="email"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="phone">Phone</InputLabel>
+          <Input
+            id="phone"
+            name="phone"
+          />
+        </FormControl>
+        <FormControl >
+          <InputLabel htmlFor="notes">Notes</InputLabel>
+          <Input
+            id="notes"
+            name="notes"
+            multiline
+            fullWidth={true}
+            rowsMax={20}
+            rows={20}
+          />
+        </FormControl>
+      </Fragment>
+    );
+  }
+
 };
 
 export default Form;
